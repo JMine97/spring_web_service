11c2ad99ae8440d7b202c1f4a6efd4808eac52fa
JM_CHOI_ <chlwjdals98@gmail.com>
2021-03-12 23:39:06 +0900

    문자열 출력 테스트

---
+++ src/test/java/spring/spring_web_service/SpringWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.spring_web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
