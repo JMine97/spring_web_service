b7d1be297f4da7f73989148bec6572c647d87d39
JM_CHOI_ <chlwjdals98@gmail.com>
2021-03-16 15:06:52 +0900

    게시글 수정/조회 API

---
+++ src/main/java/spring/spring_web_service/web/dto/PostsResponseDto.java
@@ -0,0 +1,22 @@
+package spring.spring_web_service.web.dto;
+
+import lombok.Getter;
+import spring.spring_web_service.domain.posts.Posts;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
+
+
