24ef0b3210e7e887a9e0ae4a581b357903cec464
JM_CHOI_ <chlwjdals98@gmail.com>
2021-03-25 14:00:11 +0900

    구글 로그인

---
+++ src/main/java/spring/spring_web_service/config/auth/LoginUser.java
@@ -0,0 +1,11 @@
+package spring.spring_web_service.config.auth;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginUser {
+}
--- src/main/java/spring/spring_web_service/web/controller/IndexController.java
+++ src/main/java/spring/spring_web_service/web/controller/IndexController.java
@@ -5,8 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import spring.spring_web_service.config.auth.LoginUser;
+import spring.spring_web_service.config.auth.dto.SessionUser;
 import spring.spring_web_service.service.posts.PostsService;
 import spring.spring_web_service.web.dto.PostsResponseDto;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
@@ -15,8 +19,12 @@ public class IndexController {
     private final PostsService postService;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postService.findAllDesc());
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
